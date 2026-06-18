e4eedfee4dbc9c995bb7899003284946be1baa95
Lari-Ribeiro-20 <lrmusic718@gmail.com>
2026-06-18 16:32:22 -0300

    Add navbar to the includes directory

---
+++ includes/navbar.php
@@ -0,0 +1,67 @@
+<!-- Navigation bar -->
+<nav class="navbar navbar-expand-lg navbar-dark">
+
+    <div class="container">
+
+        <!-- Brand logo -->
+        <a class="navbar-brand fw-bold" href="/techminds-education/index.php">
+            TechMinds Education
+        </a>
+
+        <!-- Mobile menu button -->
+        <button class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#menu">
+
+            <span class="navbar-toggler-icon"></span>
+
+        </button>
+
+        <!-- Navigation menu -->
+        <div class="collapse navbar-collapse" id="menu">
+
+            <ul class="navbar-nav ms-auto">
+
+                <!-- Home link -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/techminds-education/index.php">
+                        Home
+                    </a>
+                </li>
+
+                <!-- About link -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/techminds-education/pages/sobre.php">
+                        Sobre
+                    </a>
+                </li>
+
+                <!-- Contact link -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/techminds-education/pages/contato.php">
+                        Contato
+                    </a>
+                </li>
+
+                <!-- Login link -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/techminds-education/pages/login.php">
+                        Entrar
+                    </a>
+                </li>
+
+                <!-- Register button -->
+                <li class="nav-item">
+                    <a class="btn btn-tech ms-2" href="/techminds-education/pages/cadastro.php">
+                        Cadastrar
+                    </a>
+                </li>
+
+            </ul>
+
+        </div>
+
+    </div>
+
+</nav>
