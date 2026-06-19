f366536f19c8f709b25bb962fe2ee9a555823496
Malu Gibrail <malu.bomdia@gmail.com>
2026-06-19 07:16:57 -0300

    Add footer section.

---
+++ includes/footer.php
@@ -0,0 +1,30 @@
+<footer class="footer">
+
+
+    <div class="container text-center">
+
+
+        <h4>TechMinds Education</h4>
+
+
+        <p>
+            Educação, tecnologia e preparação para o ENEM.
+        </p>
+
+
+        <p>
+            © 2026 - Todos os direitos reservados.
+        </p>
+
+
+    </div>
+
+
+</footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+</html>
