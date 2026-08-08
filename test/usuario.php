3cd0cd96447a8ed8fdea8e03b82d3ef9439e6072
Lari-Ribeiro-20 <lrmusic718@gmail.com>
2026-08-08 19:27:11 -0300

    Add test for Usuario model functionality

---
+++ test/usuario.php
@@ -0,0 +1,11 @@
+<?php
+
+/* =========================================
+   TEST USER MODEL
+========================================= */
+
+require_once '../models/Usuario.php';
+
+$usuario = new Usuario();
+
+echo "Model Usuario funcionando corretamente!";
