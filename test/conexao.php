fbc59a1824e100ab38f1023d14ca20744e937f83
Lari-Ribeiro-20 <lrmusic718@gmail.com>
2026-08-08 19:26:36 -0300

    Add test for database connection

---
+++ test/conexao.php
@@ -0,0 +1,9 @@
+<?php
+
+/* =========================================
+   TEST DATABASE CONNECTION
+========================================= */
+
+require_once '../config/conexao.php';
+
+echo "Conexão com o banco realizada com sucesso!";
