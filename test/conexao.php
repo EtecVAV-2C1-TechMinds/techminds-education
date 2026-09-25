bda1a599f475ad2a8cd1b875a7760343d53543c5
Isabella Fernanda <isa.fernanda.251109@gmail.com>
2026-09-24 23:51:26 -0300

    Remove comments from conexao.php

--- test/conexao.php
+++ test/conexao.php
@@ -1,9 +1,4 @@
 <?php
 
-/* =========================================
-   TEST DATABASE CONNECTION
-========================================= */
-
 require_once '../config/conexao.php';
-
 echo "Conexão com o banco realizada com sucesso!";
